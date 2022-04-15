3fbb0df83e13a5a6a6017d7ac4383f216eab6c2f
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-15 16:30:42 +0200

    media queries added

--- src/Homepage/SubHeader/styled.js
+++ src/Homepage/SubHeader/styled.js
@@ -7,20 +7,37 @@ export const Wrapper = styled.div`
     flex-direction: column;
     align-items: center;
     margin: 72px 0 0 0;
+
+    @media(max-width: ${() => theme.breakpoints.mobileMax}px){
+        margin: 48px 0 0 0;
+    }
 `;
 
 export const Icon = styled(GithubIcon)`
     width: 40px;
+
+    @media(max-width: ${() => theme.breakpoints.mobileMax}px){
+        width: 32px;
+    }
 `;
 
 export const Title = styled.h2`
     font-size: 30px;
     font-weight: 900;
     margin: 13px 0 8px 0;
+
+    @media(max-width: ${() => theme.breakpoints.mobileMax}px){
+        font-size: 18px;
+        margin: 13px 0 16px 0;
+    }
 `;
 
 export const Description = styled.p`
     font-size: 20px;
     color: ${() => theme.colors.mineShaft};
     margin: 0 0 24px 0;
+
+    @media(max-width: ${() => theme.breakpoints.mobileMax}px){
+        font-size: 17px;
+    }
 `;
