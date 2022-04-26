535f2ef9c95fe498b50b4fc4b6c5ff40661e7062
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 22:21:18 +0200

    dispatching action moved to homepage component

--- src/Homepage/Tile/index.js
+++ src/Homepage/Tile/index.js
@@ -1,40 +1,14 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchRepositories, resetState, selectError, selectLoading, selectRepositories } from "../homepageSlice";
 import { Wrapper, Title, Tile, Description, LinksSection, Link, LinksDescription } from "./styled";
 
-
-
-export const TilesSection = () => {
-    const dispatch = useDispatch();
-
-    const data = useSelector(selectRepositories);
-    const isLoading = useSelector(selectLoading);
-    const isError = useSelector(selectError);
-
-    useEffect(() => {
-        dispatch(fetchRepositories());
-        return () => resetState();
-    }, [dispatch])
-
+export const TilesSection = ({ title, description, demoLink, codeLink }) => {
     return (
         <>
             <Wrapper>
                 <Tile>
-                    <Title>Movies Browser</Title>
-                    <Description>Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.</Description>
-                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link target="_blank" rel="noreferrer" href="">wejdztu.pl</Link></LinksSection>
-                    <LinksSection><LinksDescription>Code:</LinksDescription><Link target="_blank" rel="noreferrer" href="">kodtu.pl</Link></LinksSection>
-                </Tile>
-                <Tile>
-                    <Title>Movies Browser</Title>
-                    <Description>Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.</Description>
-                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link>wejdztu.pl</Link></LinksSection>
-                </Tile>
-                <Tile>
-                    <Title>Movies Browser</Title>
-                    <Description>Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.</Description>
-                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link>wejdztu.pl</Link></LinksSection>
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link target="_blank" rel="noreferrer" href="">{demoLink}</Link></LinksSection>
+                    <LinksSection><LinksDescription>Code:</LinksDescription><Link target="_blank" rel="noreferrer" href="">{codeLink}</Link></LinksSection>
                 </Tile>
             </Wrapper>
         </>
