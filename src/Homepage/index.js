78210716441ba6acf8d778077ca56262022f344e
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 23:25:25 +0200

    dispatch moved to tile component

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -1,6 +1,3 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchRepositories, resetState, selectError, selectLoading, selectRepositories } from "./homepageSlice";
 import { Header } from "./Header";
 import { Skills } from "./Section";
 import { futureSkillsData, skillsData } from "./skillsData";
@@ -10,26 +7,14 @@ import { TilesSection } from "./Tile";
 import { Footer } from "./Footer";
 
 export const Homepage = () => {
-    const dispatch = useDispatch();
 
-    const data = useSelector(selectRepositories);
-    const isLoading = useSelector(selectLoading);
-    const isError = useSelector(selectError);
-
-    useEffect(() => {
-        dispatch(fetchRepositories());
-        return () => resetState();
-    }, [dispatch])
-
-    html_url: 
-    name:
     return (
         <Wrapper>
             <Header />
             <Skills title={"My skillset includes"} skills={skillsData} />
             <Skills title={"What I want to learn next"} skills={futureSkillsData} />
             <SubHeader />
-            <TilesSection title={"Movies Browser"} description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."} demoLink={"wejdztu.pl"} codeLink={"kodtu.pl"} />
+               <TilesSection />
             <Footer />
         </Wrapper>
     )
