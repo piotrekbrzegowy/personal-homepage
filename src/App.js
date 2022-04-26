fcfd8b42e8ad0c23342e7856d2bfd85ca537859a
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 21:46:25 +0200

    store provider added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import store from "./store";
 import { GlobalStyle } from "./GlobalStyle";
 import { Homepage } from "./Homepage";
 import { theme } from "./theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Homepage />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Homepage />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
