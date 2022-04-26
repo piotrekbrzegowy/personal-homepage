b46cc8a5f5dd1e48ee4e97188149e15e02dd9603
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 22:21:35 +0200

    dispatching action fetchRepositories

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRepositories, resetState, selectError, selectLoading, selectRepositories } from "./homepageSlice";
 import { Header } from "./Header";
 import { Skills } from "./Section";
 import { futureSkillsData, skillsData } from "./skillsData";
@@ -7,13 +10,26 @@ import { TilesSection } from "./Tile";
 import { Footer } from "./Footer";
 
 export const Homepage = () => {
+    const dispatch = useDispatch();
+
+    const data = useSelector(selectRepositories);
+    const isLoading = useSelector(selectLoading);
+    const isError = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+        return () => resetState();
+    }, [dispatch])
+
+    html_url: 
+    name:
     return (
         <Wrapper>
             <Header />
             <Skills title={"My skillset includes"} skills={skillsData} />
             <Skills title={"What I want to learn next"} skills={futureSkillsData} />
             <SubHeader />
-            <TilesSection />
+            <TilesSection title={"Movies Browser"} description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."} demoLink={"wejdztu.pl"} codeLink={"kodtu.pl"} />
             <Footer />
         </Wrapper>
     )
