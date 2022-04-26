0e5c69dce39de6778156816e0ee7027b11050058
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 21:50:52 +0200

    dispatching action fetchRepositories

--- src/Homepage/Tile/index.js
+++ src/Homepage/Tile/index.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRepositories, resetState, selectError, selectLoading, selectRepositories } from "../homepageSlice";
 import { Wrapper, Title, Tile, Description, LinksSection, Link, LinksDescription } from "./styled";
 
+
+
 export const TilesSection = () => {
+    const dispatch = useDispatch();
+
+    const data = useSelector(selectRepositories);
+    const isLoading = useSelector(selectLoading);
+    const isError = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+        return () => resetState();
+    }, [dispatch])
+
     return (
         <>
             <Wrapper>
