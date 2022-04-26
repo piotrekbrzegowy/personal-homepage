ff75cd3b869f0beae89cf094a8c87d409bee6044
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-26 23:25:54 +0200

    dispatching fetchRepositories action and mapping

--- src/Homepage/Tile/index.js
+++ src/Homepage/Tile/index.js
@@ -1,15 +1,31 @@
 import { Wrapper, Title, Tile, Description, LinksSection, Link, LinksDescription } from "./styled";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRepositories, resetState, selectError, selectLoading, selectRepositories } from "../homepageSlice";
 
-export const TilesSection = ({ title, description, demoLink, codeLink }) => {
+export const TilesSection = () => {
+
+    const dispatch = useDispatch();
+
+    const data = useSelector(selectRepositories);
+    const isLoading = useSelector(selectLoading);
+    const isError = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+        return () => resetState();
+    }, [dispatch])
+
     return (
         <>
-            <Wrapper>
-                <Tile>
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link target="_blank" rel="noreferrer" href="">{demoLink}</Link></LinksSection>
-                    <LinksSection><LinksDescription>Code:</LinksDescription><Link target="_blank" rel="noreferrer" href="">{codeLink}</Link></LinksSection>
+            <Wrapper>{data.map(({ id, name, description, homepage, html_url }) => (
+                <Tile key={id && id}>
+                    <Title>{name && name}</Title>
+                    <Description>{description && description}</Description>
+                    <LinksSection><LinksDescription>Demo:</LinksDescription><Link target="_blank" rel="noreferrer" href={homepage}>{homepage && homepage}</Link></LinksSection>
+                    <LinksSection><LinksDescription>Code:</LinksDescription><Link target="_blank" rel="noreferrer" href={html_url}>{html_url && html_url}</Link></LinksSection>
                 </Tile>
+            ))}
             </Wrapper>
         </>
     )
