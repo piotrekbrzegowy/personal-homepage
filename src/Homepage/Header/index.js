da7b3e5ac7ad1e1ef3b4e792d14c6f628728d6ea
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-27 20:01:41 +0200

    button changed to link, added mail to property

--- src/Homepage/Header/index.js
+++ src/Homepage/Header/index.js
@@ -1,5 +1,6 @@
-import { Wrapper, Image, SmallText, Title, Content, Description, Button, ButtonIcon } from "./styled";
+import { Wrapper, Image, SmallText, Title, Content, Description, ButtonLink, ButtonIcon } from "./styled";
 import profilePhoto from "./img/photo.jpg";
+import { email } from "../email";
 
 export const Header = () => {
 	return (
@@ -10,7 +11,7 @@ export const Header = () => {
 					<SmallText>This is</SmallText>
 					<Title>Piotrek Brzegowy</Title>
 					<Description>I'm a passionate Junior Frontend Developer, currently looking for new job opportunities.</Description>
-					<Button><ButtonIcon/>Hire me</Button>
+					<ButtonLink href={`mailto:${email}`} title={email}><ButtonIcon/>Hire me</ButtonLink>
 				</Content>
 			</Wrapper>
 		</>
