f0e67d767cb5b29120096895cbcada75621738ff
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-04-27 19:58:41 +0200

    title changed to link

--- src/Homepage/Footer/index.js
+++ src/Homepage/Footer/index.js
@@ -1,12 +1,13 @@
+import { email } from "../email";
 import { SocialIcons } from "./SocialIcons";
-import { Description, SmallText, Title, Wrapper } from "./styled";
+import { Description, SmallText, TitleLink, Wrapper } from "./styled";
 
 export const Footer = () => {
     return (
         <>
             <Wrapper>
                 <SmallText>LET'S TALK!</SmallText>
-                <Title>brzegowypiotrek@gmail.com</Title>
+                <TitleLink href={`mailto: ${email}`}>brzegowypiotrek@gmail.com</TitleLink>
                 <Description>I’m always open to new projects whenever I have the time. If you want to create a website and need some help to make your ideas come to life, feel free to contact me</Description>
                 <SocialIcons />
             </Wrapper>
