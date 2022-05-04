096dbc2bad520af1af21c951adca09be3b681e59
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2022-05-04 21:46:41 +0200

    theme slice added

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import homepageSlice from "./Homepage/homepageSlice";
 import rootSaga from "./rootSaga";
+import themeSlice from "./themeSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
     reducer: {
         homepage: homepageSlice,
+        theme: themeSlice,
     },
     middleware: [sagaMiddleware],
 })
